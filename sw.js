701d870388639c1a8511762ee9c8a43dfdfd5779
Zack Backmen <backmen@bk.ru>
2021-06-22 16:44:41 +0300

    fix

--- sw.js
+++ sw.js
@@ -54,3 +54,20 @@ self.addEventListener('fetch', (e) => {
     return response;
   })());
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(_cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+          return cacheName != _cacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
